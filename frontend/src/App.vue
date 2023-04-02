7bf45c739e26d317aa387c153da44445cf99a751
lederluc <lederluc@students.zhaw.ch>
2023-04-02 17:31:02 +0200

    #4 Basic navigation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,46 +1,20 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Navbar from './components/Navbar.vue'
 
 console.log(1)
 </script>
 
 <template>
     <div class="container">
-      <HelloWorld />
+      <Header />
     <RouterView></RouterView>
     </div>
-  <div class="button-container text-center">
-    <div class="button-group">
-      <div class="col p-3 border bg-light">
-        <a href="/List">List</a>
-      </div>
-      <div class="col p-3 border bg-light">
-        <a href="/Scanner">Scan</a>
-      </div>
-      <div class="col p-3 border bg-light">
-        <a href="/Options">options</a>
-      </div>
-    </div>
-
-  </div>
+  <Navbar></Navbar>
 
 </template>
 
 <style lang="scss" scoped>
-.button-container {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-}
-
-.button-group {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-}
 
 </style>
