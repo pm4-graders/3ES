4b6585d0d924b246f3ac80a11565a9759bd92340
wehrltob <=>
2023-04-13 16:06:47 +0200

    wip - yahtzee copy

--- cv/DigitRecognizer.py
+++ cv/DigitRecognizer.py
@@ -60,67 +60,17 @@ class DigitRecognizer:
 
         #Find mask
         mask = np.zeros((gray.shape),np.uint8)
-        cv2.drawContours(mask,[best_cnt],0,255,5)
+        cv2.drawContours(mask,[best_cnt],0,255,7)
         cv2.drawContours(mask,[best_cnt],0,255,-1)
 
         #Apply mask to copy of original image
         out = np.zeros_like(gray)
         out[mask == 255] = gray[mask == 255]
 
-        #Apply same transformation to grid
-        blur = cv2.GaussianBlur(out, (3,3), 0)
-        thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,57,5)
-        contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        # Filter out all numbers and noise to isolate only boxes
-        for c in contours:
-            area = cv2.contourArea(c)
-            # If area is really small, draw over it.
-            if area < 1000:
-                cv2.drawContours(thresh, [c], -1, (0,0,0), -1)
+        self.get_yatzy_grid(out)
 
-        cv2.imshow('thresh', thresh)
-        cv2.waitKey(0)
-
-        # Fix horizontal and vertical lines
-        vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1,5))
-        thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, vertical_kernel, iterations=6)
-        horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,1))
-        thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, horizontal_kernel, iterations=6)
-
-        cv2.imshow('thresh', thresh)
-        cv2.waitKey(0)
-
-        # Sort by top to bottom and each row by left to right
-        invert = 255 - thresh
-        cnts = cv2.findContours(invert, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-        (cnts, _) = imutils_contours.sort_contours(cnts, method="top-to-bottom")
-
-        sudoku_rows = []
-        row = []
-        for (i, c) in enumerate(cnts, 1):
-            area = cv2.contourArea(c)
-            if area < 50000:
-                row.append(c)
-                if i % 9 == 0:  
-                    (cnts, _) = imutils_contours.sort_contours(row, method="left-to-right")
-                    sudoku_rows.append(cnts)
-                    row = []
-
-        # Iterate through each box
-        for row in sudoku_rows:
-            for c in row:
-                mask = np.zeros(image.shape, dtype=np.uint8)
-                cv2.drawContours(mask, [c], -1, (255,255,255), -1)
-                result = cv2.bitwise_and(image, mask)
-                result[mask==0] = 255
-                cv2.imshow('result', result)
-                cv2.waitKey(0)
-
-
-        cv2.imshow("Final Image", image)
-
+        
     def find_largest_contours(self, contours):
         max_area = 0
         best_cnt = 0
@@ -133,3 +83,184 @@ class DigitRecognizer:
                             best_cnt = i
                 c+=1
         return best_cnt
+    
+    def get_yatzy_grid(self, img_binary_sheet):
+        """ Returns a binary image with a grid and the input image containing only horizontal/vertical lines.
+        Args:
+            img_binary_sheet ((rows,col) array): binary image
+        Returns:
+            img_binary_grid: an image containing painted vertically and horizontally lines
+            img_binary_sheet_only_digits: an image containing only(mostly) handwritten digits
+        """
+        height, width = img_binary_sheet.shape
+
+        img_binary_sheet_morphed = img_binary_sheet.copy()
+
+        # Now we have the binary image with adaptive threshold.
+        # We need to do some morphylogy operations in order to strengthen thin lines, remove noise, and also handwritten stuff.
+        # We only want the horizontal / vertical pixels left before we start identifying the grid. See http://homepages.inf.ed.ac.uk/rbf/HIPR2/morops.htm
+
+        # CLOSING: (dilate -> erode) will fill in background (black) regions with White. Imagine sliding struct element
+        # in the background pixel, if it cannot fit the background completely(touching the foreground), fill this pixel with white
+
+        # OPENING: ALL FOREGROUND PIXELS(white) that can fit the structelement will be white, else black.
+        # Erode -> Dilate
+
+        # Erosion: If the structuring element can fit inside the forground pixel(white), then keep white, else set to black
+        # Dilation: For every background pixel(black), if one of the foreground(white) pixels are present, set this background (black) to foreground.
+
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+        img_binary_sheet_morphed = cv2.morphologyEx(img_binary_sheet_morphed, cv2.MORPH_DILATE, kernel)
+
+        cv2.imshow("morph", img_binary_sheet_morphed)
+        cv2.waitKey(0)
+
+        sheet_binary_grid_horizontal = img_binary_sheet_morphed.copy()
+        sheet_binary_grid_vertical = img_binary_sheet_morphed.copy()
+
+        # We use relative length for the structuring line in order to be dynamic for multiple sizes of the sheet.
+        structuring_line_size = int(width / 5.0)
+
+        # Try to remove all vertical stuff in the image,
+        element = cv2.getStructuringElement(cv2.MORPH_RECT, (structuring_line_size, 1))
+        sheet_binary_grid_horizontal = cv2.morphologyEx(sheet_binary_grid_horizontal, cv2.MORPH_OPEN, element)
+
+        # Try to remove all horizontal stuff in image, Morph OPEN: Keep everything that fits structuring element i.e vertical lines
+        element = cv2.getStructuringElement(cv2.MORPH_RECT, (1, structuring_line_size))
+
+        sheet_binary_grid_vertical = cv2.morphologyEx(sheet_binary_grid_vertical, cv2.MORPH_OPEN, element)
+
+        # Concatenate the vertical/horizontal lines into grid
+        img_binary_sheet_morphed = cv2.add(
+            sheet_binary_grid_vertical, sheet_binary_grid_horizontal)
+
+        cv2.imshow("morph_keep_only_horizontal_lines", sheet_binary_grid_horizontal)
+        cv2.waitKey(0)
+
+        # cv_utils.show_window("morph_keep_only_horizontal_lines",
+        #                     sheet_binary_grid_horizontal)
+        # cv_utils.show_window("morph_keep_only_vertical_lines", sheet_binary_grid_vertical)
+        # cv_utils.show_window("concatenate_vertical_horizontal", img_binary_sheet_morphed)
+        # """
+        #     Time to get a solid grid, from what we see  above, the grid is still not fully filled (sometimes)
+        #     since the paper is not fully straight on the table etc. For this we use Hough Transform
+        #     Hough transform identifies points (x,y) on the same line. 
+        # """
+
+        # We ideally should choose np.pi / 2 for the Theta accumulator, since we only want lines in 90 degrees and 0 degrees.
+        rho_accumulator = 1
+        angle_accumulator = np.pi / 2
+        # Min vote for defining a line
+        threshold_accumulator_votes = int(width/2)
+
+        # Find lines in the image according to the Hough Algorithm
+        grid_lines = cv2.HoughLines(img_binary_sheet_morphed, rho_accumulator,
+                                    angle_accumulator, threshold_accumulator_votes)
+
+        img_binary_grid = np.zeros(
+            img_binary_sheet_morphed.shape, dtype=img_binary_sheet_morphed.dtype)
+
+        # Since we can have multiple lines for same grid line, we merge nearby lines
+        grid_lines = self.merge_nearby_lines(grid_lines)
+
+        self.draw_lines(grid_lines, img_binary_grid)
+
+        # Since all sheets does not have outerborders. We draw a rectangle around the
+        outer_border = np.array([
+            [1, height-1],  # Bottom Left
+            [1, 1],  # Top Left
+            [width-1, 1],  # Top Right
+            [width-1, height-1]  # Bottom Right
+        ])
+        cv2.drawContours(img_binary_grid, [outer_border], 0, (255, 255, 255), 3)
+
+        # Remove the grid from the binary image an keep only the digits.
+        img_binary_sheet_only_digits = cv2.bitwise_and(img_binary_sheet, 255 - img_binary_sheet_morphed)
+
+        cv2.imshow("yatzy_grid_binary_lines", img_binary_grid)
+        cv2.waitKey(0)
+        
+        return img_binary_grid, img_binary_sheet_only_digits
+
+    def draw_lines(self, lines, img):
+        if lines is not None:
+            for line in lines:
+                rho, theta = line[0]
+                a = np.cos(theta)
+                b = np.sin(theta)
+                x0 = a * rho
+                y0 = b * rho
+                pt1 = (int(x0 + 10000*(-b)), int(y0 + 10000*(a)))
+                pt2 = (int(x0 - 10000*(-b)), int(y0 - 10000*(a)))
+                cv2.line(img, pt1, pt2,
+                        (255, 255, 255), 2)
+        
+        
+    def merge_nearby_lines(self, lines, rho_distance=30, degree_distance=20):
+        """ Merges nearby lines with the specified rho and degree distance.
+        Args:
+            lines (list): A list of lines (rho, theta), see OpenCV HoughLines
+            rho_distance (int): Distance in rho for lines to merge (default is 30)
+            degree_distacne (int): Distance in degrees for lines to merge (default is 20)
+        Returns:
+            list: a list of estimated lines
+        """
+
+        lines = lines if lines is not None else []
+        estimated_lines = []
+        for line in lines:
+            if line is False:
+                continue
+
+            estimated_line = self.get_merged_line(
+                lines, line, rho_distance, degree_distance)
+            estimated_lines.append(estimated_line)
+
+        return estimated_lines
+
+    def get_merged_line(self, lines, line_a, rho_distance, degree_distance):
+        """ Merges all line in lines with the distance to line_a iteratively.
+        Returns:
+            list: a list of estimated lines
+        """
+        for i, line_b in enumerate(lines):
+            if line_b is False:
+                continue
+            if self.__should_merge_lines(line_a, line_b, rho_distance, degree_distance):
+                # Update line A on every iteration
+                line_a = self.merge_lines(line_a, line_b)
+                # Don't use B again
+                lines[i] = False
+
+        return line_a
+
+
+
+    def __should_merge_lines(self, line_a, line_b, rho_distance, theta_distance):
+        rho_a, theta_a = line_a[0].copy()
+        rho_b, theta_b = line_b[0].copy()
+        if(rho_b == rho_a and theta_b == theta_b):
+            return False
+
+        # Use degree for more intuitive user format
+        theta_b = int(180 * theta_b / np.pi)
+        theta_a = int(180 * theta_a / np.pi)
+
+        # In Q3 or Q4, See merge_lines method
+        if rho_b < 0:
+            theta_b = theta_b - 180
+
+        # In Q3 or Q4, See merge_lines method
+        if rho_a < 0:
+            theta_a = theta_a - 180
+
+        rho_a = np.abs(rho_a)
+        rho_b = np.abs(rho_b)
+
+        diff_theta = np.abs(theta_a - theta_b)
+        rho_diff = np.abs(rho_a - rho_b)
+
+        if(rho_diff < rho_distance and diff_theta < theta_distance):
+            return True
+
+        return False
--- cv/app.py
+++ cv/app.py
@@ -4,8 +4,8 @@ from DigitRecognizer import DigitRecognizer
 
 #image = cv2.imread("C:/Users/TLAP/source/repos/ZHAW/3ES/cv/testImages/white_with_watermark.jpg")
 #image = cv2.imread("C:/Users/TLAP/source/repos/ZHAW/3ES/cv/testImages/small_nrs.png")
-#image = cv2.imread("C:/Users/TLAP/source/repos/ZHAW/3ES/cv/testImages/kanti_img1.jpg")
-image = cv2.imread("C:/Users/TLAP/source/repos/ZHAW/3ES/cv/testImages/kanti_img2.jpg")
+image = cv2.imread("C:/Users/TLAP/source/repos/ZHAW/3ES/cv/testImages/kanti_img1.jpg")
+#image = cv2.imread("C:/Users/TLAP/source/repos/ZHAW/3ES/cv/testImages/kanti_img2.jpg")
 #image = cv2.imread("C:/Users/TLAP/source/repos/ZHAW/3ES/cv/testImages/corner_outside_frame.jpg",1)
 
 
