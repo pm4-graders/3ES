d2410e106232205aab49cf9af3ab10d3778c2a50
Dominic <21215549+LordBrud@users.noreply.github.com>
2023-04-20 10:02:30 +0200

    removed kandidatennummer, geburtsdatum. Added qr_code. Changed to English. Removed example call

--- cv/Config.py
+++ cv/Config.py
@@ -1,15 +1,6 @@
 class Config:
     def __init__(self):
-        self.kandidatennummer = {'top_left': (0, 0), 'bottom_right': (0, 0)}
-        self.geburtsdatum = {'top_left': (0, 0), 'bottom_right': (0, 0)}
-        self.aufgaben = {'top_left': (0, 0), 'bottom_right': (0, 0)}
-        self.erreichte_punktzahl = {'top_left': (0, 0), 'bottom_right': (0, 0)}
-        self.schlussnote = {'top_left': (0, 0), 'bottom_right': (0, 0)}
-
-
-coords = Config()
-coords.kandidatennummer['top_left'] = (10, 20)
-coords.kandidatennummer['bottom_right'] = (50, 60)
-
-print(coords.kandidatennummer)
-# Output: {'top_left': (10, 20), 'bottom_right': (50, 60)}
+        self.exercises = {'top_left': (0, 0), 'bottom_right': (0, 0)}
+        self.achieved_score = {'top_left': (0, 0), 'bottom_right': (0, 0)}
+        self.final_grade = {'top_left': (0, 0), 'bottom_right': (0, 0)}
+        self.qr_code = {'top_left': (0, 0), 'bottom_right': (0, 0)}
