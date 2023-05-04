1d8ce35dfd5939f1fd870d1a5edf19dcb65bfe1c
Tobias Wehrli <twehrli@techgroup.ch>
2023-05-04 18:21:27 +0200

    simple tests

--- cv/app.py
+++ cv/app.py
@@ -13,15 +13,19 @@ images.append(cv2.imread("cv/testImages/perspective.jpg"))
 images.append(cv2.imread("cv/testImages/crooked.jpg"))
 
 
-recognizer = DigitRecognizer(True)
+recognizer = DigitRecognizer(False)
 #cv2.imshow('image', image)
 for index, image in enumerate(images):
     start = time.time()
     try:
-        exam = recognizer.recognize_digits_in_photo(image)
-        print("IMAGE" + str(index+1) + "PASSED")
+        cv_result = recognizer.recognize_digits_in_photo(image)
+        print("Result validated: " + str(cv_result.result_validated))
+
+        print(', '.join([str(exercise.score) for exercise in cv_result.exam.exercises]) + " | Total: " + str(cv_result.exam.score))
+
+        print("IMAGE " + str(index+1) + " PASSED")
     except:
-        print("IMAGE" + str(index+1) + "DID NOT PASS")
+        print("IMAGE " + str(index+1) + " DID NOT PASS")
         pass
     end = time.time()
     print(end - start)
