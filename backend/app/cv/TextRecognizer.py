06262fdf8f4998fe948b8713a6661efd46b6ae26
wehrltob <=>
2023-05-25 15:54:47 +0200

    add config and grab all info

--- backend/app/cv/TextRecognizer.py
+++ backend/app/cv/TextRecognizer.py
@@ -11,9 +11,12 @@ class TextRecognizer:
         # initialize any variables
         global DEBUG_MODE
         DEBUG_MODE = debug_mode
+
+        self.reader = Reader(["de"], gpu=0)
+
         pass
 
-    def recognize_text(self, image):
+    def recognize_text(self, image, config):
         """
         Recognizes exam info text in a photo.
 
@@ -23,34 +26,73 @@ class TextRecognizer:
         Returns:
         The recognized text fields as string array
         """
-        
-        #Cut off top half of image
-        image = image[:int(image.shape[0]/2), :]
+        global DEBUG_MODE
+
+        if DEBUG_MODE:
+            cv2.imshow("orig", cv2.resize(image, (int(image.shape[1] / 3), int(image.shape[0] / 3))))
+            cv2.waitKey(0)
 
         #Convert image to grayscale
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         #Resize the grid to scale to our wanted reference
-        width = gray.shape[1]
-        wanted_width = 2000
-        scale_percent = wanted_width / width
+        gray = cv2.resize(gray, (1400, 2000), interpolation = cv2.INTER_AREA)
 
-        gray = cv2.resize(gray, (int(gray.shape[1] * scale_percent), int(gray.shape[0] * scale_percent)))
+        #Cut off top half of image as we don't need it
+        gray = gray[:int(gray.shape[0]/2), :]
 
-        reader = Reader(["de"], gpu=0)
-        results = reader.readtext(gray, min_size=100, width_ths=1)
+        recognized = [None] * 5
 
-        recognized = [None] * 3
+        for attribute, value in config.__dict__.items():
 
-        for result in results:
-            text = result[1]
-            if("Prüf. " in text):
-                recognized[0] = int(''.join(filter(str.isdigit, text)))
-            if("Kand. " in text):
-                recognized[1] = int(''.join(filter(str.isdigit, text)))
-            if("Geb. " in text or "Dat." in text):
-                date = parser.parse(text, fuzzy=True, dayfirst=True)
-                recognized[2] = date
+            x,y = value['top_left']
+            x1,y1 = value['bottom_right']
+
+            input = gray[y:y1,x:x1]
+
+            if DEBUG_MODE:
+                cv2.imshow("cut", cv2.resize(input, (int(input.shape[1]), int(input.shape[0]))))
+                cv2.waitKey(0)
+
+            results = self.reader.readtext(input, min_size=100, width_ths=1.3)
+            
+            for result in results:
+                text = result[1]
+            
+                if attribute == "sticker":
+                    if("Prüf. " in text):
+                        recognized[0] = int(''.join(filter(str.isdigit, text)))
+                    if("Kand. " in text):
+                        recognized[1] = int(''.join(filter(str.isdigit, text)))
+                    if("Geb. " in text or "Dat." in text):
+                        try:
+                            date = parser.parse(text, fuzzy=True, dayfirst=True)
+                        except:
+                            date = None
+                        recognized[2] = date
+                
+                if attribute == "year_with_text":
+                    if("Aufnahme" in text):
+                        recognized[3] = int(''.join(filter(str.isdigit, text)))
+                if attribute == "test_name":
+                    if not("ohne" in text or "Tasch" in text or "rechner" in text):
+                        recognized[4] = text
 
         return recognized
 
+    def recognize_grid_number(self, image):
+        """
+        Recognizes a grid number.
+
+        Parameters:
+        Image: cv2.imread or similarly parsed image object
+
+        Returns:
+        The recognized text fields as string array
+        """
+        res = self.reader.readtext(image, min_size=10)
+        if len(res) != 0:
+            res = int(''.join(filter(str.isdigit, res[0][1])))
+        else:
+            res = None
+        return res
