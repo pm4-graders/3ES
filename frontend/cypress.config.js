99fe069cbe7d8c49475855911f2d0c87661ac5d0
Dominic <21215549+LordBrud@users.noreply.github.com>
2023-06-23 14:47:54 +0200

    ci: trying to fix frontend testing

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'https://localhost:5173'
+    baseUrl: 'https://localhost:4173'
   }
 })
